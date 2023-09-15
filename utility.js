edfd9b1290c4ce808584cb6540d2f50386a767b0
Md Wasif Jawed <106733153+red1mist@users.noreply.github.com>
2023-09-16 03:08:32 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,22 @@
+let utility = {};
+utility.types = {
+  media: ["mp4", "mkv"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+    "tex",
+  ],
+  images: ["jpg", "jpeg", "png"],
+  gifs: ["gif"],
+};
+module.exports = utility;
